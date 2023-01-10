1fd069922dc5e64f82153005b2e5b78b414991ff
ridwan rmd <ridwan95rmd@gmail.com>
2023-01-10 14:12:58 +0700

    generate model Product, table name: products

---
+++ migrations/20230110070454-create-product.js
@@ -0,0 +1,41 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Products", {
+      productId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      productName: {
+        allowNull: false,
+        type: Sequelize.STRING(100),
+      },
+      desc: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal(
+          "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
+        ),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Products");
+  },
+};
---
+++ models/product.js
@@ -0,0 +1,43 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Product.init(
+    {
+      productId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      productName: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      desc: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      price: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Product",
+      tableName: "products",
+    }
+  );
+  return Product;
+};
