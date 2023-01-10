5940e4f63f952a03c365a7ca8433f3177ac20a29
ridwan rmd <ridwan95rmd@gmail.com>
2023-01-10 13:49:17 +0700

    create new test nodejs PT JCS project, setup database and setup sequelize orm

---
+++ config/config.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+require("dotenv").config();
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = rpocces.env;
+
+module.exports = {
+  development: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: 3306,
+    dialect: "mysql",
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
+  },
+};
